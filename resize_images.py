40bade6a1df0c8bac036b16d987534edd42d9a51
Tommy Ngx <tommyfederation@gmail.com>
2023-12-08 19:47:12 +1100

    Update resize_images.py

--- resize_images.py
+++ resize_images.py
@@ -2,23 +2,28 @@ import os
 import cv2
 import argparse
 from tqdm import tqdm
+import shutil
 
 def resize_images(source_folder, destination_folder, target_size):
-    os.makedirs(destination_folder, exist_ok=True)
-
     for root, dirs, files in os.walk(source_folder):
-        for file in tqdm(files, desc="Processing images", unit="image"):
+        relative_path = os.path.relpath(root, source_folder)
+        destination_path = os.path.join(destination_folder, relative_path)
+
+        for file in tqdm(files, desc=f"Processing images in {relative_path}", unit="image"):
             if file.lower().endswith(('.png', '.jpg', '.jpeg', '.dcm')):
+                source_image_path = os.path.join(root, file)
+                destination_image_path = os.path.join(destination_path, file)
+
+                os.makedirs(destination_path, exist_ok=True)
+
                 # Load the image
-                image_path = os.path.join(root, file)
-                img = cv2.imread(image_path)
+                img = cv2.imread(source_image_path)
 
                 # Resize the image
                 resized_img = cv2.resize(img, target_size)
 
                 # Save the resized image
-                destination_path = os.path.join(destination_folder, file)
-                cv2.imwrite(destination_path, resized_img)
+                cv2.imwrite(destination_image_path, resized_img)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Resize images in a folder and its subfolders.")
