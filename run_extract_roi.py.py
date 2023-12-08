db86aa9cbb7b1b89046007f6a2fd7e58670a20fc
Tommy Ngx <tommyfederation@gmail.com>
2023-12-08 17:58:17 +1100

    Update run_extract_roi.py.py

--- run_extract_roi.py.py
+++ run_extract_roi.py.py
@@ -1,19 +1,17 @@
 import os
 import argparse
 import cv2
-import numpy as np
+import pandas as pd
 from tqdm import tqdm
 import pydicom
 from extract_roi import extract_ROI  # Assuming you have the extract_ROI class in a separate file
 
 def main(args):
     # Load YOLO model
-    # Replace the following line with the code to load your YOLO model
     yolo_model = load_yolo_model(args.yolo_model_path)
 
-    # Load DataFrame or any other data structure you are using
-    # Replace the following line with the code to load your DataFrame
-    df = load_dataframe(args.dataframe_path)
+    # Automatically create a DataFrame from the images folder
+    df = create_dataframe(args.images_folder)
 
     # Initialize extract_ROI object
     extractor = extract_ROI(df, yolo_model)
@@ -31,15 +29,14 @@ def load_yolo_model(model_path):
     # Example: yolo_model = your_yolo_loading_function(model_path)
     return None  # Change this line
 
-def load_dataframe(dataframe_path):
-    # Replace this with your DataFrame loading code
-    # Example: df = your_dataframe_loading_function(dataframe_path)
-    return None  # Change this line
+def create_dataframe(images_folder):
+    image_files = [f for f in os.listdir(images_folder) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.dcm'))]
+    df = pd.DataFrame({"Path": [os.path.join(images_folder, img) for img in image_files]})
+    return df
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run extract_ROI on images and masks.")
     parser.add_argument("--yolo_model_path", required=True, help="Path to the YOLO model.")
-    parser.add_argument("--dataframe_path", required=True, help="Path to the DataFrame or data structure.")
     parser.add_argument("--images_folder", required=True, help="Path to the folder containing images.")
     parser.add_argument("--masks_folder", required=True, help="Path to the folder containing masks.")
     parser.add_argument("--output_folder", required=True, help="Path to the output folder for processed images and masks.")
