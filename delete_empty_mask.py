d5d3bef07b95d8880a5320e5ccb69846b1ff5e58
Tommy Ngx <tommyfederation@gmail.com>
2023-12-10 10:57:47 +1100

    Update delete_empty_mask.py

--- delete_empty_mask.py
+++ delete_empty_mask.py
@@ -9,13 +9,25 @@ from tqdm import tqdm
 from PIL import Image
 import numpy as np
 
-def is_empty_mask(mask_path):
-    # Function to check if a mask is empty (all pixel values are 0)
-    with Image.open(mask_path) as img:
-        array = np.array(img)
-        return not array.any()
+import os
+from tqdm import tqdm
+from PIL import Image
+import numpy as np
 
-def delete_empty_masks(images_folder, masks_folder):
+def calculate_coverage(mask_path, threshold=0.1):
+    with Image.open(mask_path) as img:
+        mask_array = np.array(img)
+
+    # Set all pixels higher than 0 to 255
+    mask_array[mask_array > 0] = 255
+
+    total_pixels = mask_array.size
+    white_pixels = np.count_nonzero(mask_array)
+    percentage_coverage = (white_pixels / total_pixels) * 100
+
+    return percentage_coverage < threshold
+
+def delete_low_coverage_masks(images_folder, masks_folder, threshold=0.1):
     image_files = os.listdir(images_folder)
     mask_files = os.listdir(masks_folder)
 
@@ -24,14 +36,13 @@ def delete_empty_masks(images_folder, masks_folder):
 
     deleted_count = 0
 
-    for file in tqdm(common_files, desc="Deleting empty masks"):
+    for file in tqdm(common_files, desc="Deleting low-coverage masks"):
         image_path = os.path.join(images_folder, file)
         mask_path = os.path.join(masks_folder, file)
 
-        if os.path.exists(mask_path) and is_empty_mask(mask_path):
+        if os.path.exists(mask_path) and calculate_coverage(mask_path, threshold):
             os.remove(mask_path)
             os.remove(image_path)  # Delete corresponding image
-
             deleted_count += 1
 
     return deleted_count
@@ -39,18 +50,20 @@ def delete_empty_masks(images_folder, masks_folder):
 def main():
     import argparse
 
-    parser = argparse.ArgumentParser(description='Delete empty masks and corresponding images.')
+    parser = argparse.ArgumentParser(description='Delete masks with low coverage and corresponding images.')
     parser.add_argument('--images_folder', help='Path to the images folder', required=True)
     parser.add_argument('--masks_folder', help='Path to the masks folder', required=True)
+    parser.add_argument('--threshold', type=float, default=0.1, help='Coverage threshold (default: 0.1)')
 
     args = parser.parse_args()
 
-    deleted_count = delete_empty_masks(args.images_folder, args.masks_folder)
+    deleted_count = delete_low_coverage_masks(args.images_folder, args.masks_folder, args.threshold)
 
     if deleted_count == 0:
-        print("No empty masks found and deleted.")
+        print("No masks with low coverage found and deleted.")
     else:
-        print(f"{deleted_count} empty masks and corresponding images deleted.")
+        print(f"{deleted_count} masks with low coverage and corresponding images deleted.")
 
 if __name__ == "__main__":
     main()
+
