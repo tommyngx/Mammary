b5d4d217ea2e47cd2db45333ef8e057634373029
Tommy Ngx <tommyfederation@gmail.com>
2023-12-10 07:03:33 +1100

    update merge masks

--- merge_mask.py
+++ merge_mask.py
@@ -2,39 +2,41 @@ import os
 import cv2
 import argparse
 from tqdm import tqdm
-import shutil
 
-def resize_images(source_folder, destination_folder, target_size):
-    for root, dirs, files in os.walk(source_folder):
-        relative_path = os.path.relpath(root, source_folder)
-        destination_path = os.path.join(destination_folder, relative_path)
+def merge_masks(input_folder, output_folder):
+     os.makedirs(output_folder, exist_ok=True)
 
-        for file in tqdm(files, desc=f"Processing images in {relative_path}", unit="image"):
-            if file.lower().endswith(('.png', '.jpg', '.jpeg', '.dcm')):
-                source_image_path = os.path.join(root, file)
-                destination_image_path = os.path.join(destination_path, file)
+     # Dictionary to store masks for each ID
+     id_masks = {}
 
-                os.makedirs(destination_path, exist_ok=True)
+     # Iterate through the input folder
+     for root, dirs, files in os.walk(input_folder):
+         for file in tqdm(files, desc="Processing masks", unit="mask"):
+             if file.lower().endswith('.png'):
+                 # Extract ID from the file name
+                 id_number = file.rsplit('_', 1)[0]  # Extract all characters before the last underscore
 
-                # Load the image
-                img = cv2.imread(source_image_path)
+                 # Load the mask
+                 mask_path = os.path.join(root, file)
+                 mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
 
-                # Resize the image
-                resized_img = cv2.resize(img, target_size)
+                 # Merge masks for the same ID
+                 if id_number in id_masks:
+                     id_masks[id_number] += mask
+                 else:
+                     id_masks[id_number] = mask
 
-                # Save the resized image
-                cv2.imwrite(destination_image_path, resized_img)
+     # Save merged masks
+     for id_number, merged_mask in id_masks.items():
+         output_path = os.path.join(output_folder, f"{id_number}.png")
+         cv2.imwrite(output_path, merged_mask)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Resize images in a folder and its subfolders.")
-    parser.add_argument("--source_folder", required=True, help="Path to the source folder containing images.")
-    parser.add_argument("--destination_folder", required=True, help="Path to the destination folder for resized images.")
-    parser.add_argument("--target_size", default=(640, 640), type=int, nargs=2, help="Target size for resizing (width height).")
+     parser = argparse.ArgumentParser(description="Merge masks based on ID.")
+     parser.add_argument("--input_folder", required=True, help="Path to the input folder containing masks.")
+     parser.add_argument("--output_folder", required=True, help="Path to the output folder for merged masks.")
 
-    args = parser.parse_args()
+     args = parser.parse_args()
 
-    # Convert target_size to a tuple
-    target_size = tuple(args.target_size)
-
-    # Resize images
-    resize_images(args.source_folder, args.destination_folder, target_size)
+     # Merge masks
+     merge_masks(args.input_folder, args.output_folder)
--- merge_mask_lesions.py
+++ merge_mask_lesions.py
@@ -4,17 +4,14 @@ import pandas as pd
 import argparse
 from tqdm import tqdm
 
-def merge_masks_with_conditions(df, input_folder, output_folder, split_df):
+def merge_masks_with_conditions(df, input_folder, output_folder):
     os.makedirs(output_folder, exist_ok=True)
 
-    # Dictionary to store the first mask for each ID
-    id_first_masks = {}
+    # Dictionary to store masks for each ID
+    id_masks = {}
 
-    # Only process a subset of the DataFrame based on the split_df value
-    start_index = 0 if split_df == 0 else len(df) // 2
-    end_index = len(df) // 2 if split_df == 0 else len(df)
-
-    for index, row in tqdm(df.iloc[start_index:end_index].iterrows(), desc="Processing masks", total=(end_index - start_index), unit="mask"):
+    # Apply intensity to all images based on the lesion_types column
+    for index, row in tqdm(df.iterrows(), desc="Applying intensity to images", total=len(df), unit="image"):
         image_id = row['image_id']
         mask_id = row['mask_id']
         lesion_type = row['lesion_types']
@@ -26,37 +23,42 @@ def merge_masks_with_conditions(df, input_folder, output_folder, split_df):
             # Load the original mask
             original_mask = cv2.imread(original_mask_path, cv2.IMREAD_GRAYSCALE)  # Intensity values [0, 255]
 
-            # Normalize intensity values to range [0, 1]
-            original_mask = original_mask / 255.0
-
-            # Merge masks based on conditions
+            # Normalize intensity values to range [0, 1] based on lesion type
             if lesion_type == 'Mass':
-                intensity = 0.75
-            elif lesion_type == 'Architecturaldistorsion':
                 intensity = 0.25
+            elif lesion_type == 'Architecturaldistorsion':
+                intensity = 0.75
             elif lesion_type == 'Asymmetry':
                 intensity = 0.50
             elif lesion_type == 'Microcalcification':
                 intensity = 1.0
 
-            # Add the first mask for each ID to the dictionary
-            if image_id not in id_first_masks:
-                id_first_masks[image_id] = original_mask * intensity
+            # Apply intensity to the image
+            original_mask = original_mask * intensity
+
+            # Merge masks for the same ID
+            id_number = mask_id.rsplit('_', 1)[0]
+            if id_number in id_masks:
+                id_masks[id_number] += original_mask
+            else:
+                id_masks[id_number] = original_mask
 
         except Exception as e:
             print(f"Error processing mask {mask_id}: {str(e)}")
 
-    # Save merged masks for each ID
-    for image_id, first_mask in tqdm(id_first_masks.items(), desc="Saving merged masks", unit="mask"):
-        output_path = os.path.join(output_folder, f"{image_id}")
-        cv2.imwrite(output_path, (first_mask * 255).astype(int))
+    # Save merged masks
+    for id_number, merged_mask in tqdm(id_masks.items(), desc="Saving merged masks", unit="mask"):
+        # Set values higher than 1 to 1
+        merged_mask[merged_mask > 1] = 1
+
+        output_path = os.path.join(output_folder, f"{id_number}.png")
+        cv2.imwrite(output_path, (merged_mask * 255).astype(int))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Merge masks based on conditions.")
     parser.add_argument("--input_folder", required=True, help="Path to the input folder containing original masks.")
     parser.add_argument("--output_folder", required=True, help="Path to the output folder for merged masks.")
     parser.add_argument("--csv_path", required=True, help="Path to the CSV file containing DataFrame information.")
-    parser.add_argument("--split_df", type=int, default=0, choices=[0, 1], help="Split the DataFrame into two parts. Use 0 for the first half, and 1 for the second half.")
 
     args = parser.parse_args()
 
@@ -64,4 +66,4 @@ if __name__ == "__main__":
     df = pd.read_csv(args.csv_path)
 
     # Merge masks based on conditions
-    merge_masks_with_conditions(df, args.input_folder, args.output_folder, args.split_df)
+    merge_masks_with_conditions(df, args.input_folder, args.output_folder)
