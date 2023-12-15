b16c9ff06561b88b84fa1b783a81eb8a00264f30
Tommy Ngx <tommyfederation@gmail.com>
2023-12-15 12:51:08 +1100

    Update copy_folder.py

--- copy_folder.py
+++ copy_folder.py
@@ -2,25 +2,29 @@ import os
 import shutil
 import argparse
 
-def copy_and_rename_folder_and_csv(input_folder, csv_file, output_folder):
-    # Create the output folder if it doesn't exist
+def copy_folder_and_csv(input_folder, csv_file, output_folder):
+    # Create output folder if it doesn't exist
     os.makedirs(output_folder, exist_ok=True)
 
-    # Copy the entire folder to the output folder
-    shutil.copytree(input_folder, os.path.join(output_folder, 'new_folder'))
+    # Copy folder X to Y
+    input_folder_path = os.path.abspath(input_folder)
+    output_folder_path = os.path.join(output_folder, "Y")
+    shutil.copytree(input_folder_path, output_folder_path, copy_function=shutil.copy2)
 
-    # Copy the CSV file to the output folder
-    shutil.copy(csv_file, os.path.join(output_folder, 'new_csv.csv'))
+    # Copy CSV file to Y
+    csv_file_path = os.path.join(output_folder, "Y", os.path.basename(csv_file))
+    shutil.copy(csv_file, csv_file_path)
 
 def main():
-    parser = argparse.ArgumentParser(description='Copy folder and CSV file to a new location with a new name.')
-    parser.add_argument('--input_folder', help='Path to the input folder', required=True)
+    parser = argparse.ArgumentParser(description='Copy folder X and a CSV file, then paste them with new names.')
+    parser.add_argument('--input_folder', help='Path to the input folder (X)', required=True)
     parser.add_argument('--csv_file', help='Path to the CSV file', required=True)
-    parser.add_argument('--output_folder', help='Path to the output folder', required=True)
+    parser.add_argument('--output_folder', help='Path to the output folder', default='output')
 
     args = parser.parse_args()
 
-    copy_and_rename_folder_and_csv(args.input_folder, args.csv_file, args.output_folder)
+    copy_folder_and_csv(args.input_folder, args.csv_file, args.output_folder)
+    print("Copy completed.")
 
 if __name__ == "__main__":
     main()
