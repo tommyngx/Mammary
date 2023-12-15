51e7cc8ef2ba50d16ab7f2a9389b7d2cd02b2f24
Tommy Ngx <tommyfederation@gmail.com>
2023-12-15 12:49:18 +1100

    Update copy_folder.py

--- copy_folder.py
+++ copy_folder.py
@@ -2,27 +2,25 @@ import os
 import shutil
 import argparse
 
-def copy_folder_and_csv(input_folder, csv_file, output_folder, new_name):
-    # Create a new folder with the specified new name
-    new_folder_path = os.path.join(output_folder, new_name)
-    os.makedirs(new_folder_path, exist_ok=True)
+def copy_and_rename_folder_and_csv(input_folder, csv_file, output_folder):
+    # Create the output folder if it doesn't exist
+    os.makedirs(output_folder, exist_ok=True)
 
-    # Copy the entire folder
-    shutil.copytree(input_folder, os.path.join(new_folder_path, os.path.basename(input_folder)))
+    # Copy the entire folder to the output folder
+    shutil.copytree(input_folder, os.path.join(output_folder, 'new_folder'))
 
-    # Copy the CSV file to the new folder
-    shutil.copy(csv_file, new_folder_path)
+    # Copy the CSV file to the output folder
+    shutil.copy(csv_file, os.path.join(output_folder, 'new_csv.csv'))
 
 def main():
-    parser = argparse.ArgumentParser(description='Copy a folder and CSV file, then paste with a new name.')
-    parser.add_argument('--input_folder', help='Path to the input folder to copy', required=True)
-    parser.add_argument('--csv_file', help='Path to the CSV file to copy', required=True)
-    parser.add_argument('--output_folder', help='Path to the output folder for the new copy', required=True)
-    parser.add_argument('--new_name', help='New name for the copied folder and CSV file', required=True)
+    parser = argparse.ArgumentParser(description='Copy folder and CSV file to a new location with a new name.')
+    parser.add_argument('--input_folder', help='Path to the input folder', required=True)
+    parser.add_argument('--csv_file', help='Path to the CSV file', required=True)
+    parser.add_argument('--output_folder', help='Path to the output folder', required=True)
 
     args = parser.parse_args()
 
-    copy_folder_and_csv(args.input_folder, args.csv_file, args.output_folder, args.new_name)
+    copy_and_rename_folder_and_csv(args.input_folder, args.csv_file, args.output_folder)
 
 if __name__ == "__main__":
     main()
