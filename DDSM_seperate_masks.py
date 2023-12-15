629d53009a1f6130e4ac7d0c179467da17a807db
Tommy Ngx <tommyfederation@gmail.com>
2023-12-15 11:15:12 +1100

    Update DDSM_seperate_masks.py

--- DDSM_seperate_masks.py
+++ DDSM_seperate_masks.py
@@ -1,39 +1,56 @@
 import os
+from tqdm import tqdm
 import shutil
 
-def separate_masks(mask_folder, output_folder_prefix):
-    mask_files = os.listdir(mask_folder)
+def separate_masks(images_folder, masks_folder, output_folder):
+    os.makedirs(output_folder, exist_ok=True)
 
-    for mask_file in mask_files:
+    mask_files = os.listdir(masks_folder)
+
+    for mask_file in tqdm(mask_files, desc="Separating masks"):
+        mask_path = os.path.join(masks_folder, mask_file)
+
         # Extract mask name without extension
-        mask_name, ext = os.path.splitext(mask_file)
+        mask_name_parts = os.path.splitext(mask_file)[0].rsplit('_', 1)
+        mask_name = mask_name_parts[0]
 
-        # Split the mask name by '_'
-        mask_name_parts = mask_name.split('_')
-
-        # Extract the suffix (e.g., '1', '2', '3', '4') from the last part of the name
+        # Extract suffix (_1, _2, _3, etc.)
         suffix = mask_name_parts[-1]
 
-        # Create output folder based on the suffix
-        output_folder = f"{output_folder_prefix}_{suffix}"
+        # Create output folder for the specific suffix
+        output_suffix_folder = os.path.join(output_folder, f"mask_{suffix}")
+        os.makedirs(output_suffix_folder, exist_ok=True)
 
-        # Create the output folder if it doesn't exist
-        os.makedirs(output_folder, exist_ok=True)
+        # Create subfolders for images and masks
+        output_images_folder = os.path.join(output_suffix_folder, "images")
+        output_masks_folder = os.path.join(output_suffix_folder, "masks")
+        os.makedirs(output_images_folder, exist_ok=True)
+        os.makedirs(output_masks_folder, exist_ok=True)
 
-        # Copy the mask to the appropriate output folder
-        output_mask_path = os.path.join(output_folder, f"{mask_name}{ext}")
-        shutil.copy(os.path.join(mask_folder, mask_file), output_mask_path)
+        # Copy mask to the corresponding masks folder
+        new_mask_name = f"{mask_name}.png"
+        new_mask_path = os.path.join(output_masks_folder, new_mask_name)
+        shutil.copy(mask_path, new_mask_path)
+
+        # Copy corresponding image to the images folder
+        for image_file in os.listdir(images_folder):
+            image_name = os.path.splitext(image_file)[0]
+            if image_name.startswith(mask_name):
+                image_path = os.path.join(images_folder, image_file)
+                new_image_path = os.path.join(output_images_folder, f"{image_name}.png")
+                shutil.copy(image_path, new_image_path)
 
 def main():
     import argparse
 
-    parser = argparse.ArgumentParser(description='Separate masks into different folders based on suffix.')
-    parser.add_argument('--mask_folder', help='Path to the input masks folder', required=True)
-    parser.add_argument('--output_folder_prefix', help='Prefix for the output folders', required=True)
+    parser = argparse.ArgumentParser(description='Separate masks into folders based on suffix (_1, _2, _3, etc.).')
+    parser.add_argument('--images_folder', help='Path to the images folder', required=True)
+    parser.add_argument('--masks_folder', help='Path to the masks folder', required=True)
+    parser.add_argument('--output_folder', help='Path to the output folder', required=True)
 
     args = parser.parse_args()
 
-    separate_masks(args.mask_folder, args.output_folder_prefix)
+    separate_masks(args.images_folder, args.masks_folder, args.output_folder)
 
 if __name__ == "__main__":
     main()
