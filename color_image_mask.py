a61539f3681577e71d1fb0de6d04b0784b2f27d1
Tommy Ngx <tommyfederation@gmail.com>
2023-12-15 15:39:00 +1100

    Update color_image_mask.py

--- color_image_mask.py
+++ color_image_mask.py
@@ -9,7 +9,7 @@ def create_color_palette(num_colors):
     colors = np.random.randint(0, 256, size=(num_colors, 3), dtype=np.uint8)
     return colors
 
-def process_mask(mask_path, images_folder, color_mapping):
+def process_mask(mask_path, images_folder, output_folder, color_mapping):
     mask_file = os.path.basename(mask_path)
     image_path = os.path.join(images_folder, mask_file)
 
@@ -20,12 +20,16 @@ def process_mask(mask_path, images_folder, color_mapping):
     # Create an RGB image with the same size as the mask
     colorized_mask = np.zeros_like(image)
 
+    # Find unique values in the mask
+    unique_values = np.unique(mask)
+    unique_values = unique_values[unique_values != 0]
+
     # Replace unique values with colors
-    for unique_value, color in color_mapping.items():
-        colorized_mask[mask == unique_value] = color
+    for unique_value in unique_values:
+        colorized_mask[mask == unique_value] = color_mapping[unique_value]
 
     # Blend the colorized mask with the original image
-    blended_image = cv2.addWeighted(image, 0.7, colorized_mask, 0.9, 0)
+    blended_image = cv2.addWeighted(image, 0.7, colorized_mask, 0.3, 0)
 
     # Save the blended image to the output folder
     output_path = os.path.join(output_folder, f"blended_{mask_file}")
@@ -37,40 +41,16 @@ def colorize_masks(images_folder, masks_folder, output_folder):
     # Create a color palette with 5 different colors
     color_palette = create_color_palette(5)
 
-    # Create an array to store unique pixel values
-    unique_values = np.empty((5,), dtype=np.uint8)
-
-    # Go through each mask to find unique values
-    mask_files = [f for f in os.listdir(masks_folder) if f.endswith('.png')]
-
-    with ThreadPoolExecutor() as executor:
-        futures = []
-        for mask_file in mask_files:
-            mask_path = os.path.join(masks_folder, mask_file)
-            future = executor.submit(find_unique_values, mask_path)
-            futures.append(future)
-
-        # Wait for all threads to finish
-        tqdm(futures, desc="Finding unique values", unit="mask")
-
-        # Process the results
-        for future in futures:
-            mask_unique_values = future.result()
-            unique_values = np.concatenate((unique_values, mask_unique_values), axis=None)
-
-    # Remove duplicate values and values equal to 0
-    unique_values = np.unique(unique_values)
-    unique_values = unique_values[unique_values != 0]
-
     # Create a dictionary to map unique values to colors
-    color_mapping = dict(zip(unique_values, color_palette))
+    color_mapping = {i + 1: color for i, color in enumerate(color_palette)}
 
     # Use ThreadPoolExecutor to parallelize the processing of masks
     with ThreadPoolExecutor() as executor:
         futures = []
+        mask_files = [f for f in os.listdir(masks_folder) if f.endswith('.png')]
         for mask_file in mask_files:
             mask_path = os.path.join(masks_folder, mask_file)
-            future = executor.submit(process_mask, mask_path, images_folder, color_mapping)
+            future = executor.submit(process_mask, mask_path, images_folder, output_folder, color_mapping)
             futures.append(future)
 
         # Wait for all threads to finish
