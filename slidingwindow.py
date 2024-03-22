f6ed909d0b692c79370314f7a1b1bc9214ebe6e6
Tommy Ngx <tommyfederation@gmail.com>
2024-03-22 18:01:52 +1100

    Update slidingwindow.py

--- slidingwindow.py
+++ slidingwindow.py
@@ -57,7 +57,7 @@ def slideprocess(input_folder, save_folder, size, overlap):
         num_splits = (image_height - overlap_pixels) // (size - overlap_pixels) #+ 1
         #print("code:",num_splits, "--",overlap_pixels,"--", image_height, "xx", image_width )
         remain  =  (image_height - overlap_pixels) % (size - overlap_pixels)
-        if remain >0.2: num_splits= num_splits+1
+        if remain >0.25: num_splits= num_splits+1
 
         #print("code:",num_splits, "--",overlap_pixels,"--", image_height, "xx", image_width )
 
